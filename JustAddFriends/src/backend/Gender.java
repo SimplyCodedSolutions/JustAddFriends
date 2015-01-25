66ca0122fd715f56a955e70c80e189247494c7b9
Eric Le Fort <ericlefort@live.com>
2015-01-25 18:30:34 -0500

    Reorganized
    src: source code package folders.
    doc: plain text files.
    resources: complicated files (i.e. pictures)

--- JustAddFriends/src/backend/Gender.java
+++
@@ -1,6 +0,0 @@
-package backend;
-/**
- * @author Eric Le Fort
- * @version 01
- */
-public enum Gender{ M, F }//Gender
--- JustAddFriends/src/backend/Glass.java
+++
@@ -1,6 +0,0 @@
-package backend;
-/**
- * @author Eric Le Fort
- * @version 01
- */
-public enum Glass{ COCKTAIL, HIGHBALL, MARTINI, ROCKS, SHOT, DROPSHOT}
